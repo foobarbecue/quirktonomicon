70e01057381aa0ba083983c9e08cf2f8f7cc4cfb
Aaron Curtis <foobarbecue@gmail.com>
2014-02-18 20:17:14 +0000

    Fix title cloud layout, and add indexes to database.

--- models.py
+++ models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class VoteCount(models.Model):
-    accessed_at = models.DateTimeField(blank=True, null=True)
+    accessed_at = models.DateTimeField(blank=True, null=True, db_index=True)
     idea_id = models.IntegerField()
     votes_count = models.IntegerField(blank=True, null=True)
     total_votes_needed = models.IntegerField(blank=True, null=True)
     considered_at = models.DateTimeField(blank=True, null=True)
-    state = models.CharField(max_length=30)
+    state = models.CharField(max_length=30, db_index=True)
     
     def __str__(self):
         return unicode(self.votes_count)
@@ -26,7 +26,7 @@ class Ideation(models.Model):
     expires_at = models.DateTimeField(blank=True, null=True)
     features = models.TextField(blank=True, null=True)
     id = models.IntegerField(primary_key=True)
-    idea_id = models.IntegerField()
+    idea_id = models.IntegerField(db_index=True)
     media = models.TextField(blank=True, null=True)
     not_chosen_at = models.DateTimeField(blank=True, null=True)
     patent_id = models.IntegerField(blank=True, null=True)
@@ -36,7 +36,7 @@ class Ideation(models.Model):
     similar_products = models.TextField(blank=True, null=True)
     solution = models.TextField(blank=True, null=True)
     state = models.CharField(max_length=30)
-    title = models.CharField(max_length=500)
+    title = models.CharField(max_length=500, db_index=True)
     total_votes_needed = models.IntegerField(blank=True, null=True)
     under_consideration = models.NullBooleanField(blank=True, null=True)
     user_id = models.IntegerField(blank=True, null=True)
@@ -64,7 +64,7 @@ class Flag(models.Model):
     kind=models.CharField(max_length=5,choices=KIND_CHOICES)
 
 class HourData(models.Model):
-    start_time=models.DateTimeField()
+    start_time=models.DateTimeField(db_index=True)
     new_votes=models.IntegerField(blank=True, null=True)
     new_ideas=models.IntegerField(blank=True, null=True)
     
